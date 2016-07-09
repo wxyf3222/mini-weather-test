70435f79c8cb9c8d79648a144ee2186c03bb3b0b
wuwenchun <496194722@qq.com>
2016-07-09 21:18:21 +0800

    first commit

---
+++ app/src/main/java/cn/edu/nwnu/bean/City.java
@@ -0,0 +1,24 @@
+package cn.edu.nwnu.bean;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by zhangqixun on 16/7/7.
+ */
+public class City {
+
+    String [] city= {"北京","太原","兰州"};
+
+    public String[] getCity() {
+        return city;
+    }
+
+    public void setCity(String[] city) {
+        this.city = city;
+    }
+
+    public void getPosition(int position)
+    {
+
+    }
+}
